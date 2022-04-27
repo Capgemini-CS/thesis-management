37cba5a0c47ce62a252527033c6eac98726c402b
AndreeaaBadea <andreea.badea@capgemini.com>
2022-04-27 15:33:57 +0300

    Added user entity

---
+++ src/main/java/com/campgemini/thesismanagement/domain/User.java
@@ -0,0 +1,54 @@
+package com.campgemini.thesismanagement.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class User implements UserDetails {
+    private long userId;
+    private String username;
+    private String password;
+    private String email;
+    private Boolean enabled;
+    private Boolean accountNonExpired;
+    private Boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private Collection<? extends GrantedAuthority> authorities = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
