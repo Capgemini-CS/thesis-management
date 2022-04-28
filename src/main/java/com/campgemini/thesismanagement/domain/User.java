55e148cc1ce661dad08ed19019844c6226c5e37c
AndreeaaBadea <andreea.badea@capgemini.com>
2022-04-28 11:43:53 +0300

    Added user entity

--- src/main/java/com/campgemini/thesismanagement/domain/User.java
+++ src/main/java/com/campgemini/thesismanagement/domain/User.java
@@ -1,20 +1,47 @@
 package com.campgemini.thesismanagement.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users_accounts")
 public class User implements UserDetails {
-    private long userId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user_account")
+    private Long idUser;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "enabled")
     private Boolean enabled;
+
+    @Column(name = "account_not_expired")
     private Boolean accountNonExpired;
+
+    @Column(name = "account_not_locked")
     private Boolean accountNonLocked;
+
+    @Column(name = "credentials_non_expired")
     private boolean credentialsNonExpired;
+
+//    @Column(name = "authorities")
     private Collection<? extends GrantedAuthority> authorities = new ArrayList<>();
 
     @Override
